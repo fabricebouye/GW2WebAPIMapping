9aadb099fe5b0ce4acf52b3e98eef5d6c7176f55
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-06-30 20:00:47 +1100

    Implemented the v2/build endpoint.

---
+++ src/api/web/gw2/mapping/v2/build/Build.java
@@ -0,0 +1,20 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.build;
+
+/**
+ * Defines a build (the build id of the game).
+ * @author Fabrice Bouyé
+ */
+public interface Build {
+    /**
+     * Gets the id of the build 
+     * @return 
+     */
+    int getId();
+}
---
+++ src/api/web/gw2/mapping/v2/build/DefaultBuild.java
@@ -0,0 +1,28 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.build;
+
+/**
+ * Default implementation of a group.
+ * @author Fabrice Bouyé
+ */
+final class DefaultBuild implements Build {
+
+    int id;
+
+    /**
+     * Creates a new empty instance.
+     */
+    DefaultBuild() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+}
