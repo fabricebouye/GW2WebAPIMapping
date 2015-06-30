81e391362a92935def3f33c188b128c94f5c2189
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-06-30 21:29:58 +1100

    Fixed javadoc.

--- src/api/web/gw2/mapping/v2/characters/equipment/EquipmentSlot.java
+++ src/api/web/gw2/mapping/v2/characters/equipment/EquipmentSlot.java
@@ -16,99 +16,98 @@ import api.web.gw2.mapping.v2.APIv2;
  */
 @APIv2(endpoint = "v2/characters/<name>/equipment", requiresAuthentication = true) // NOI18N.
 public enum EquipmentSlot {
+
     /**
-    * Defines the underwater breather.
-    *//**
-    * Defines the underwater breather.
-    */
+     * Defines the underwater breather.
+     */
     HELM_AQUATIC("HelmAquatic"), // NOI18N.
     /**
-    * Defines the back item.
-    */
+     * Defines the back item.
+     */
     BACKPACK("Backpack"), // NOI18N.
     /**
-    * Defines the coat (breast plate).
-    */
+     * Defines the coat (breast plate).
+     */
     COAT("Coat"), // NOI18N.
     /**
-    * Defines the boots.
-    */
+     * Defines the boots.
+     */
     BOOTS("Boots"), // NOI18N.
     /**
-    * Defines the gloves.
-    */
+     * Defines the gloves.
+     */
     GLOVES("Gloves"), // NOI18N.
     /**
-    * Defines the helm.
-    */
+     * Defines the helm.
+     */
     HELM("Helm"), // NOI18N.
     /**
-    * Defines the leggings.
-    */
+     * Defines the leggings.
+     */
     LEGGINGS("Leggings"), // NOI18N.
     /**
-    * Defines the shoulders.
-    */
+     * Defines the shoulders.
+     */
     SHOULDERS("Shoulders"), // NOI18N.
     /**
-    * Defines accessory #1.
-    */
+     * Defines accessory #1.
+     */
     ACCESSORY_1("Accessory1"), // NOI18N.
     /**
-    * Defines accessory #2.
-    */
+     * Defines accessory #2.
+     */
     ACCESSORY_2("Accessory2"), // NOI18N.
     /**
-    * Defines ring #1.
-    */
+     * Defines ring #1.
+     */
     RING_1("Ring1"), // NOI18N.
     /**
-    * Defines ring #2.
-    */
+     * Defines ring #2.
+     */
     RING_2("Ring2"), // NOI18N.
     /**
-    * Defines the amulet.
-    */
+     * Defines the amulet.
+     */
     AMULET("Amulet"), // NOI18N.
     /**
-    * Defines underwater weapon #1.
-    */
+     * Defines underwater weapon #1.
+     */
     WEAPON_AQUATIC_A("WeaponAquaticA"), // NOI18N.
     /**
-    * Defines underwater weapon #2.
-    */
+     * Defines underwater weapon #2.
+     */
     WEAPON_AQUATIC_B("WeaponAquaticB"), // NOI18N.
     /**
-    * Defines surface weapon #1 (right hand).
-    */
+     * Defines surface weapon #1 (right hand).
+     */
     WEAPON_A1("WeaponA1"), // NOI18N.
     /**
-    * Defines surface weapon #1 (left hand).
-    */
+     * Defines surface weapon #1 (left hand).
+     */
     WEAPON_A2("WeaponA2"), // NOI18N.
     /**
-    * Defines surface weapon #2 (right hand).
-    */    
+     * Defines surface weapon #2 (right hand).
+     */
     WEAPON_B1("WeaponB1"), // NOI18N.
     /**
-    * Defines surface weapon #2 (left hand).
-    */
+     * Defines surface weapon #2 (left hand).
+     */
     WEAPON_B2("WeaponB2"), // NOI18N.
     /**
-    * Defines gathering sickle.
-    */
+     * Defines gathering sickle.
+     */
     SICKLE("Sickle"), // NOI18N.
     /**
-    * Defines gathering axe.
-    */
+     * Defines gathering axe.
+     */
     AXE("Axe"), // NOI18N.
     /**
-    * Defines gathering pick.
-    */
+     * Defines gathering pick.
+     */
     PICK("Pick"), // NOI18N.
     /**
-    * Fail safe value.
-    */
+     * Fail safe value.
+     */
     @ImplementationSpecific
     UNKNOWN(null);
 
