d63fe162f919788b0b9b9a10d4d92fa6bc37d673
Fabrice Bouyé <fabriceb@spc.int>
2015-07-01 16:01:38 +1100

    Added missing javadoc.

--- src/api/web/gw2/mapping/v2/items/ItemUpgradeComponentFlag.java
+++ src/api/web/gw2/mapping/v2/items/ItemUpgradeComponentFlag.java
@@ -17,28 +17,94 @@ import api.web.gw2.mapping.v2.APIv2;
 @APIv2(endpoint = "v2/items") // NOI18N.
 public enum ItemUpgradeComponentFlag {
 
+    /**
+     * Defines the axe flag.
+     */
     AXE("Axe"), // NOI18N.
+    /**
+     * Defines the long bow flag.
+     */
     LONG_BOW("LongBow"), // NOI18N.
+    /**
+     * Defines the dagger flag.
+     */
     DAGGER("Dagger"), // NOI18N.
+    /**
+     * Defines the focus flag.
+     */
     FOCUS("Focus"), // NOI18N.
+    /**
+     * Defines the greatsword flag.
+     */
     GREATSWORD("Greatsword"), // NOI18N.
+    /**
+     * Defines the hammer flag.
+     */
     HAMMER("Hammer"), // NOI18N.
+    /**
+     * Defines the harpoon flag.
+     */
     HARPOON("Harpoon"), // NOI18N.
+    /**
+     * Defines the mace flag.
+     */
     MACE("Mace"), // NOI18N.
+    /**
+     * Defines the pistol flag.
+     */
     PISTOL("Pistol"), // NOI18N.
+    /**
+     * Defines the rifle flag.
+     */
     RIFLE("Rifle"), // NOI18N.
+    /**
+     * Defines the scepter flag.
+     */
     SCEPTER("Scepter"), // NOI18N.
+    /**
+     * Defines the shield flag.
+     */
     SHIELD("Shield"), // NOI18N.
-    SHORT_BOW("ShortBox"), // NOI18N.
+    SHORT_BOW("ShortBow"), // NOI18N.
+    /**
+     * Defines the speargun flag.
+     */
     SPEARGUN("Speargun"), // NOI18N.
+    /**
+     * Defines the staff flag.
+     */
     STAFF("Staff"), // NOI18N.
+    /**
+     * Defines the sword flag.
+     */
     SWORD("Sword"), // NOI18N.
+    /**
+     * Defines the torch flag.
+     */
     TORCH("Torch"), // NOI18N.
+    /**
+     * Defines the trident flag.
+     */
     TRIDENT("Trident"), // NOI18N.
+    /**
+     * Defines the warhorn flag.
+     */
     WARHORN("Warhorn"), // NOI18N.
+    /**
+     * Defines the heavy armor flag.
+     */
     HEAVY_ARMOR("HeavyArmor"), // NOI18N.
+    /**
+     * Defines the medium armor flag.
+     */
     MEDIUM_ARMOR("MediumArmor"), // NOI18N.
+    /**
+     * Defines the light armor flag.
+     */
     LIGHT_ARMOR("LightArmor"), // NOI18N.
+    /**
+     * Defines the trinket flag.
+     */
     TRINKET("Trinket"), // NOI18N.
     /**
      * Fail safe value.
