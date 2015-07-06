e0d33da685306e6dfa094b154f81212c91e45d33
Fabrice Bouyé <fabriceb@spc.int>
2015-07-06 16:31:46 +1100

    Added a quaggan factory.

---
+++ src/api/web/gw2/mapping/v2/quaggans/QuagganFactory.java
@@ -0,0 +1,27 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.quaggans;
+
+import api.web.gw2.mapping.v2.quaggans.Quaggan;
+import java.io.IOException;
+
+/**
+ * Defines a factory that produces a quaggan object.
+ * @author Fabrice Bouyé
+ */
+@FunctionalInterface
+public interface QuagganFactory {
+
+    /**
+     * Creates a new quaggan instance.
+     * @param basecode The source URL.
+     * @return A {@code Quaggan} instance, never {@code null}.
+     * @throws IOException In case of IO error.
+     */
+    Quaggan create(final String basecode) throws IOException;
+}
