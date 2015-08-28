5f3488a5148bd06fe83579924ff665d180084a5f
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-08-28 20:23:48 +1100

    Added preliminary support for the v2/currencies endpoint.

---
+++ src/api/web/gw2/mapping/v2/currencies/Currency.java
@@ -0,0 +1,51 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.currencies;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines a currency.
+ * @author Fabrice Bouyé
+ */
+@APIv2
+public interface Currency {
+
+    /**
+     * Gets the id of this currency.
+     * @return An {@code int}
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the localized name of this currency.
+     * @return A {@code String} instance, never {@code null}
+     */
+    String getName();
+
+    /**
+     * Gets the localized description of this currency.
+     * @return A {@code String} instance, never {@code null}
+     */
+    String getDescription();
+
+    /**
+     * Gets the URL of the icon of this currency.
+     * @return A {@code String} instance, never {@code null}
+     */
+    String getIcon();
+
+    /**
+     * Gets the sorting order of this currency.
+     * @return An {@code int}
+     */
+    @IdValue
+    int getOrder();
+}
