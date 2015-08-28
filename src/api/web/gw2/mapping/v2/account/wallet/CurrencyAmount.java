8a104a2137328bf6fcd8603b0121d4ae5a1fec54
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-08-28 20:34:15 +1100

    Added preliminary support for the v2/account/wallet endpoint.

---
+++ src/api/web/gw2/mapping/v2/account/wallet/CurrencyAmount.java
@@ -0,0 +1,33 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.account.wallet;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines a currency quantity in the account wallet.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/account/wallet", requiresAuthentication = true) // NOI18N.
+public interface CurrencyAmount {
+    /**
+     * The id of the currency.
+     * @return An {@code int}.
+     */
+    @IdValue
+    public int getId();
+    
+    /**
+     * Gets the amount of this currency.
+     * @return An {@code int}.
+     */
+    @QuantityValue
+    int getValue();
+}
