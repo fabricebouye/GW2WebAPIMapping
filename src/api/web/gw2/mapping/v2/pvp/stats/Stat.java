e712da2b0bfdbbd48939222ff15a725fa2e96e72
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-09-08 17:47:58 +1100

    Added preliminary support for the v2/pvp/stats endpoint.

---
+++ src/api/web/gw2/mapping/v2/pvp/stats/Stat.java
@@ -0,0 +1,49 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.pvp.stats;
+
+import api.web.gw2.mapping.v2.APIv2;
+import api.web.gw2.mapping.v2.characters.CharacterProfession;
+import api.web.gw2.mapping.v2.pvp.PvPLadder;
+import java.util.Map;
+
+/**
+ * Defines the PvP stats of an account.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/pvp/stats", requiresAuthentication = true) // NOI18N.
+public interface Stat {
+
+    /**
+     * Gets the PvP rank of the account.
+     * @return An {@code int}
+     */
+    int getPvpRank();
+
+    /**
+     * Gets the aggregate result for the account.
+     * @return A {@code StatResult}, never {@code null}.
+     */
+    StatResult getAggregate();
+
+    /**
+     * Gets the result for each ingame profession for the account.
+     * @return A non-modifiable {@code Map<CharacterProfession, StatResult>}, never {@code null}.
+     * <br/>Professions that haven't been played in PvP, will have their result set to {@code StatResult.EMPTY}.
+     * @see StatResult#EMPTY
+     */
+    Map<CharacterProfession, StatResult> getProfessions();
+
+    /**
+     * Gets the result for each ingame ladder mode for the account.
+     * @return A non-modifiable {@code Map<PvPLadder, StatResult>}, never {@code null}.
+     * <br/>Ladder modes that haven't been played in PvP, will have their result set to {@code StatResult.EMPTY}.
+     * @see StatResult#EMPTY
+     */
+    Map<PvPLadder, StatResult> getLadders();
+}
---
+++ src/api/web/gw2/mapping/v2/pvp/stats/StatResult.java
@@ -0,0 +1,107 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.pvp.stats;
+
+import api.web.gw2.mapping.core.ImplementationSpecific;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines a win/loss object.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/pvp/stats", requiresAuthentication = true) // NOI18N.
+public interface StatResult {
+
+    /**
+     * The empty stats result singleton.
+     */
+    @ImplementationSpecific
+    public static final StatResult EMPTY = new StatResult() {
+
+        /**
+         * {@inheritDoc}
+         * @return Always 0.
+         */
+        @Override
+        public int getWins() {
+            return 0;
+        }
+
+        /**
+         * {@inheritDoc}
+         * @return Always 0.
+         */
+        @Override
+        public int getLosses() {
+            return 0;
+        }
+
+        /**
+         * {@inheritDoc}
+         * @return Always 0.
+         */
+        @Override
+        public int getDesertions() {
+            return 0;
+        }
+
+        /**
+         * {@inheritDoc}
+         * @return Always 0.
+         */
+        @Override
+        public int getByes() {
+            return 0;
+        }
+
+        /**
+         * {@inheritDoc}
+         * @return Always 0.
+         */
+        @Override
+        public int getForfeits() {
+            return 0;
+        }
+    };
+
+    /**
+     * Gets the number of wins for the category.
+     * @return An {@code int} &ge; 0.
+     */
+    @QuantityValue
+    int getWins();
+
+    /**
+     * Gets the number of losses for the category.
+     * @return An {@code int} &ge; 0.
+     */
+    @QuantityValue
+    int getLosses();
+
+    /**
+     * Gets the number of desertions for the category.
+     * @return An {@code int} &ge; 0.
+     */
+    @QuantityValue
+    int getDesertions();
+
+    /**
+     * Gets the number of byes for the category.
+     * @return An {@code int} &ge; 0.
+     */
+    @QuantityValue
+    int getByes();
+
+    /**
+     * Gets the number of forfeits for the category.
+     * @return An {@code int} &ge; 0.
+     */
+    @QuantityValue
+    int getForfeits();
+}
