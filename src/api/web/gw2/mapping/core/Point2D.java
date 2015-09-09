3262455571b79db2f3ddb1fd4a19adff0c3677e9
Fabrice Bouyé <fabriceb@spc.int>
2015-09-09 13:55:11 +1100

    Added some more javadoc.

--- src/api/web/gw2/mapping/core/Point2D.java
+++ src/api/web/gw2/mapping/core/Point2D.java
@@ -11,7 +11,8 @@ import java.util.Objects;
 
 /**
  * Defines a 2D point.
- * <br>Defined a point class so we do not rely on either AWT or JavaFX.
+ * <br>Defined a point class so we do not rely on either AWT, Java2D or JavaFX.
+ * <br/>Instance of this class are not mutable.
  * @author Fabrice Bouyé
  * @param <T> The type to use.
  */
