4c105df1fdd58211d68bbf27be024e2502c8ecce
Fabrice Bouyé <fabriceb@spc.int>
2015-12-01 15:18:41 +1100

    Provided with a constants that covers all valid in game levels. Provided default implementation for equality and hash code. Fixed spelling.

--- src/api/web/gw2/mapping/v2/achievements/daily/DailyAchievementLevelRange.java
+++ src/api/web/gw2/mapping/v2/achievements/daily/DailyAchievementLevelRange.java
@@ -7,7 +7,9 @@
  */
 package api.web.gw2.mapping.v2.achievements.daily;
 
+import api.web.gw2.mapping.core.ImplementationSpecific;
 import api.web.gw2.mapping.core.LevelValue;
+import java.util.Objects;
 
 /**
  * Defines a level range for daily achievements.
@@ -16,14 +18,63 @@ import api.web.gw2.mapping.core.LevelValue;
 public interface DailyAchievementLevelRange {
 
     /**
-     * Gets the min level reqirement.
+     * Defines a level range for all valid character levels in the game.
+     */
+    @ImplementationSpecific
+    public static final DailyAchievementLevelRange ALL_LEVELS = new DailyAchievementLevelRange() {
+        @Override
+        public boolean equals(Object obj) {
+            boolean result = false;
+            if (obj instanceof DailyAchievementLevelRange) {
+                result = equals((DailyAchievementLevelRange) obj);
+            }
+            return result;
+        }
+
+        @Override
+        public int hashCode() {
+            return hashValues();
+        }
+
+        @Override
+        public int getMin() {
+            return LevelValue.MIN_LEVEL;
+        }
+
+        @Override
+        public int getMax() {
+            return LevelValue.MAX_LEVEL;
+        }
+    };
+
+    /**
+     * Provides a default implementation of the {@code equals()} method.
+     * @param other Another object.
+     * @throws NullPointerException If {@code other} is {@code null}.
+     * @return {@code True} if the test is verified, {@code false} otherwise.
+     */
+    default boolean equals(DailyAchievementLevelRange other) throws NullPointerException {
+        Objects.requireNonNull(other);
+        final boolean result = (getMin() == other.getMin()) && (getMax() == other.getMax());
+        return result;
+    }
+
+    default int hashValues() {
+        int hash = 3;
+        hash = 29 * hash + getMin();
+        hash = 29 * hash + getMax();
+        return hash;
+    }
+
+    /**
+     * Gets the min level requirement.
      * @return An {@code int}.
      */
     @LevelValue
     int getMin();
 
     /**
-     * Gets the max level reqirement.
+     * Gets the max level requirement.
      * @return An {@code int}.
      */
     @LevelValue
