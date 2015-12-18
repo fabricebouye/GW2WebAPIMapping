d20a5bfc02409cd15731a2e969f0e430dc8da1ef
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-12-18 22:02:40 +1100

    Added permissions ids gathered from testing.

--- src/api/web/gw2/mapping/v2/guild/permissions/PermissionId.java
+++ src/api/web/gw2/mapping/v2/guild/permissions/PermissionId.java
@@ -16,19 +16,45 @@ import api.web.gw2.mapping.v2.APIv2;
  */
 @APIv2(endpoint = "v2/guild/permissions") // NOI18N.
 public enum PermissionId {
+    ACTIVATE_PLACEABLES("ActivatePlaceables"),
+    ACTIVATE_WORLD_EVENT("ActivateWorldEvent"),
     /**
      * Allows to administrate lower ranks.
      */
     ADMIN("Admin"),
+    CLAIMABLE_ACTIVATE("ClaimableActivate"),
+    CLAIMABLE_CLAIM("ClaimableClaim"),
     /**
      * Allows to edit guild-owned claimables.
      */
     CLAIMABLE_EDIT_OPTIONS("ClaimableEditOptions"),
+    CLAIMABLE_SPEND("ClaimableSpend"),
+    DECORATION_ADMIN("DecorationAdmin"),
+    DEPOSIT_COINS_STASH("DepositCoinsStash"),
+    DEPOSIT_COINS_TROVE("DepositCoinsTrove"),
+    DEPOSIT_ITEMS_TROVE("DepositItemsTrove"),
+    DEPOSIT_ITEM_STASH("DepositItemsStash"),
+    EDIT_ASSEMBLY_QUEUE("EditAssemblyQueue"),
     /**
      * Allows to edit guild anthem.
      */
     EDIT_ANTHEM("EditAnthem"),
-    // @todo Query the endpoint to find out more!
+    EDIT_BGM("EditBGM"),
+    EDIT_EMBLEM("EditEmblem"),
+    EDIT_MOTD("EditMOTD"),
+    EDIT_ROLES("EditRoles"),
+    MISSION_CONTROL("MissionControl"),
+    OPEN_PORTAL("OpenPortal"),
+    PLACE_ARENA_DECORATION("PlaceArenaDecoration"),
+    PLACE_DECORATION("PlaceDecoration"),
+    PURCHASE_UPGRADES("PurchaseUpgrades"),
+    SET_GUILD_HALL("SetGuildHall"),
+    SPEND_FUEL("SpendFuel"),
+    TEAM_ADMIN("TeamAdmin"),
+    WITHDRAW_COINS_STASH("WithdrawCoinsStash"),
+    WITHDRAW_COINS_TROVE("WithdrawCoinsTrove"),
+    WITHDRAW_ITEMS_STASH("WithdrawItemsStash"),
+    WITHDRAW_ITEMS_TROVE("WithdrawItemsTrove"),
     /**
      * Fail safe value.
      */
