599e9d50da3068339501e1e1118be6057db6e7bd
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-12-19 22:15:34 +1100

    Checked endpoint result on wiki.

--- src/api/web/gw2/mapping/v2/guild/id/members/Member.java
+++ src/api/web/gw2/mapping/v2/guild/id/members/Member.java
@@ -7,6 +7,8 @@
  */
 package api.web.gw2.mapping.v2.guild.id.members;
 
+import api.web.gw2.mapping.core.DateValue;
+import api.web.gw2.mapping.core.IdValue;
 import api.web.gw2.mapping.v2.APIv2;
 import java.time.ZonedDateTime;
 
@@ -17,9 +19,24 @@ import java.time.ZonedDateTime;
 @APIv2(endpoint = "v2/guid/:id/ranks", requiresAuthentication = true, scope = "guilds") // NOI18N.
 public interface Member {
 
+    /**
+     * Gets the name of the account.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @IdValue
     String getName();
 
+    /**
+     * Gets the name of the rank.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @IdValue
     String getRank();
 
-    ZonedDateTime getJoinDate();
+    /**
+     * Gets date this member joined the guild.
+     * @return A {@code ZonedDateTime} instance, never {@code null}.
+     */
+    @DateValue
+    ZonedDateTime getJoined();
 }
