6233816fdb34ab11df8a3c523bc0acd0930d19f9
Fabrice Bouyé <fabrice.bouye@gmail.com>
2016-02-27 17:11:01 +1100

    Added support for the v2/achievements/groups endpoint.

---
+++ src/api/web/gw2/mapping/v2/achievements/groups/Group.java
@@ -0,0 +1,57 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.achievements.groups;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.v2.APIv2;
+import java.util.Set;
+
+/**
+ * Defines an achievement category group.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/achievements/categories") // NOI18N.
+public interface Group {
+
+    /**
+     * Gets the id of this category group.
+     * @return An {@code int} &gt: 0.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the localized name of this category group.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getName();
+
+    /**
+     * Gets the localized description of this category group.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getDescription();
+
+    /**
+     * Gets the order of this category group.
+     * @return An {@code int} &ge: 0.
+     */
+    @QuantityValue
+    int getOrder();
+
+    /**
+     * Gets the list of achievments categories ids in this group.
+     * @return A non-modifiable {@code Set<Integer>} instance, never {@code null}.
+     */
+    @IdValue
+    Set<Integer> getCategories();
+}
