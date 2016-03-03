732265a39a21e73d3a3ea9198304ef3ffa1404b9
Fabrice Bouyé <fabriceb@spc.int>
2016-03-03 12:00:24 +1100

    Added an empty singleton instance. Added missing annotations.

--- src/api/web/gw2/mapping/v2/guild/id/teams/TeamLadderResult.java
+++ src/api/web/gw2/mapping/v2/guild/id/teams/TeamLadderResult.java
@@ -16,34 +16,69 @@ import api.web.gw2.mapping.v2.APIv2;
  */
 @APIv2(endpoint = "v2/guid/:id/teams", requiresAuthentication = true, scope = "guilds") // NOI18N.
 public interface TeamLadderResult {
+
+    /**
+     * The singleton empty instance.
+     */
+    public static final TeamLadderResult EMPTY = new TeamLadderResult() {
+        @Override
+        public int getWins() {
+            return 0;
+        }
+
+        @Override
+        public int getLosses() {
+            return 0;
+        }
+
+        @Override
+        public int getDesertions() {
+            return 0;
+        }
+
+        @Override
+        public int getByes() {
+            return 0;
+        }
+
+        @Override
+        public int getForfeits() {
+            return 0;
+        }
+    };
+
     /**
      * Gets the number of wins.
      * @return An {@code int} &ge; 0
      */
     @QuantityValue
     int getWins();
-    
+
     /**
      * Gets the number of losses.
      * @return An {@code int} &ge; 0
      */
+    @QuantityValue
     int getLosses();
-    
+
     /**
      * Gets the number of desertions.
      * @return An {@code int} &ge; 0
      */
+    @QuantityValue
     int getDesertions();
-    
+
     /**
      * Gets the number of byes.
      * @return An {@code int} &ge; 0
      */
+    @QuantityValue
     int getByes();
-    
+
     /**
      * Gets the number of forfeits.
      * @return An {@code int} &ge; 0
      */
+    @QuantityValue
     int getForfeits();
 }
