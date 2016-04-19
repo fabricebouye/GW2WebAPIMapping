3ec384548d73912a0bb221f7645aa463eca95a1c
Fabrice Bouyé <fabriceb@spc.int>
2016-04-20 09:01:23 +1100

    Added support for the v2/account/inventory endpoint.

---
+++ src/api/web/gw2/mapping/v2/account/inventory/SharedInventory.java
@@ -0,0 +1,45 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.account.inventory;
+
+import api.web.gw2.mapping.v2.characters.inventory.InventoryBinding;
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines an inventory slot.
+ * @author Fabrice Bouyé
+ * @see InventoryBag
+ */
+@APIv2(endpoint = "v2/account/inventory", requiresAuthentication = true, scope = "inventories") // NOI18N.
+public interface SharedInventory {
+
+    /**
+     * Gets the Id of the object in this inventory slot.
+     * @return An {@code int}.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the number of this object in this inventory slot.
+     * @return An {@code int}.
+     */
+    @QuantityValue
+    int getCount();
+
+    /**
+     * Gets the binding status of this inventory slot.
+     * @return Always {@code InventoryBinding.ACCOUNT}.
+     * @see InventoryBinding#ACCOUNT
+     */
+    default InventoryBinding getBinding() {
+        return InventoryBinding.ACCOUNT;
+    }
+}
