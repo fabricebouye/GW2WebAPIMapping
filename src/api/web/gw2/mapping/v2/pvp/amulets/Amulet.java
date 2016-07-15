7c8922ecc64179c44eb9657cd51bd8ffc03d84bc
Fabrice Bouyé <fabriceb@spc.int>
2016-07-15 14:17:38 +1100

    Added preliminary support for v2/pvp/amulets.

---
+++ src/api/web/gw2/mapping/v2/pvp/amulets/Amulet.java
@@ -0,0 +1,52 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.pvp.amulets;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.MapValue;
+import api.web.gw2.mapping.core.URLReference;
+import api.web.gw2.mapping.core.URLValue;
+import api.web.gw2.mapping.v2.APIv2;
+import java.util.Map;
+
+/**
+ * Defines all PvP ladder types.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/pvp/amulets")
+public interface Amulet {
+
+    /**
+     * Gets the id of this PvP amulet.
+     * @return A {@code String}, never {@code null}.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the localized name of this PvP amulet.
+     * @return A {@code String}, never {@code null}.
+     */
+    @LocalizedResource
+    String getName();
+
+    /**
+     * Gets the URL to the icon of this PvP amulet.
+     * @return A {@code URLReference}, never {@code null}.
+     */
+    @URLValue
+    URLReference getIcon();
+
+    /**
+     * Gets the attributes values for this PvP amulet.
+     * @return A non-modifiable {@code Map<AmuletAttribute, Integer>}, never {@code null}.
+     */
+    @MapValue
+    Map<AmuletAttribute, Integer> getAttributes();
+}
---
+++ test/api/web/gw2/mapping/v2/pvp/amulets/AmuletsUtilsTest.java
@@ -0,0 +1,81 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.pvp.amulets;
+
+import api.web.gw2.mapping.core.EnumValueFactory;
+import java.util.stream.IntStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test.
+ * @author Fabrice Bouyé
+ */
+public class AmuletsUtilsTest {
+
+    public AmuletsUtilsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of AmuletAttribute.
+     */
+    @Test
+    public void testFindAmuletAttribute() {
+        System.out.println("findAmuletAttribute");
+        final String[] values = {
+            "ConditionDamage", // NOI18N.
+            "CritDamage", // NOI18N.
+            "Healing", // NOI18N.
+            "Power", // NOI18N.
+            "Precision", // NOI18N.
+            "Toughness", // NOI18N.
+            "Vitality", // NOI18N.
+            null,
+            "" // NOI18N.
+        };
+        final AmuletAttribute[] expResults = {
+            AmuletAttribute.CONDITION_DAMAGE,
+            AmuletAttribute.FEROCITY,
+            AmuletAttribute.HEALING,
+            AmuletAttribute.POWER,
+            AmuletAttribute.PRECISION,
+            AmuletAttribute.TOUGHNESS,
+            AmuletAttribute.VITALITY,
+            AmuletAttribute.UNKNOWN,
+            AmuletAttribute.UNKNOWN
+        };
+        assertEquals(values.length, expResults.length);
+        IntStream.range(0, values.length).
+                forEach(index -> {
+                    final String value = values[index];
+                    final AmuletAttribute expResult = expResults[index];
+                    final AmuletAttribute result = EnumValueFactory.INSTANCE.mapEnumValue(AmuletAttribute.class, value);
+                    assertEquals(expResult, result);
+                });
+    }
+}
