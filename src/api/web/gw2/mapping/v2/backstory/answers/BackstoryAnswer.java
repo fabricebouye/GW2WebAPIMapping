eaf989f87ff0c1929bbc092f01a06210f6b83b87
Fabrice Bouyé <fabriceb@spc.int>
2016-09-16 09:33:08 +1100

    Added  support for v2/backstory/answers.

---
+++ src/api/web/gw2/mapping/v2/backstory/answers/BackstoryAnswer.java
@@ -0,0 +1,73 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.backstory.answers;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.SetValue;
+import api.web.gw2.mapping.v2.characters.CharacterProfession;
+import api.web.gw2.mapping.v2.characters.CharacterRace;
+import java.util.Set;
+
+/**
+ * Defines a backstory (character's biography) answer.
+ * @author Fabrice Bouyé
+ */
+public interface BackstoryAnswer {
+
+    /**
+     * Gets the id of this answer.
+     * @return An {@code int}.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the i18n title of this answer.
+     * @return A {@code String}, never {@code null}.
+     */
+    @LocalizedResource
+    String getTitle();
+
+    /**
+     * Gets the i18n description of this answer.
+     * @return A {@code String}, never {@code null}.
+     */
+    @LocalizedResource
+    String getDescription();
+
+    /**
+     * Gets the i18n journal entry of this answer.
+     * @return A {@code String}, never {@code null}.
+     */
+    @LocalizedResource
+    String getJournal();
+
+    /**
+     * Gets the id of the parent question of this answer.
+     * @return An {@code int}.
+     */
+    @IdValue
+    int getQuestion();
+
+    /**
+     * Gets the professions for which this answer is valid.
+     * @return A non-modifiable {@code  Set<CharacterProfession>}, never {@code null}.
+     * @see api.web.gw2.mapping.v2.characters.CharacterProfession
+     */
+    @SetValue
+    Set<CharacterProfession> getProfessions();
+
+    /**
+     * Gets the races for which this answer is valid.
+     * @return A non-modifiable {@code Set<CharacterRace>}, never {@code null}.
+     * @see api.web.gw2.mapping.v2.characters.CharacterRace
+     */
+    @SetValue
+    Set<CharacterRace> getRace();
+}
