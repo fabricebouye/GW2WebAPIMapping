527d6afd9a817992db26a99808c9d99b7b1bc529
Fabrice Bouyé <fabriceb@spc.int>
2016-09-16 10:03:53 +1100

    Added support for v2/pets.

---
+++ src/api/web/gw2/mapping/v2/pets/Pet.java
@@ -0,0 +1,48 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.pets;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.URLReference;
+import api.web.gw2.mapping.core.URLValue;
+
+/**
+ * Defines a ranger's pet.
+ * @author Fabrice Bouyé
+ */
+public interface Pet {
+
+    /**
+     * Gets the id of this pet.
+     * @return An {@code int}.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the i18n name of this pet.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getName();
+
+    /**
+     * Gets the i18n description of this pet.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getDescription();
+
+    /**
+     * Gets the URL to the icon of this pet.
+     * @return A {@code URLReference} instance, never {@code null}.
+     */
+    @URLValue
+    URLReference getIcon();
+}
