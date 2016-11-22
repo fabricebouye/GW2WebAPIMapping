061bed2d7d0629ae80ad1f36b6795ede98e9e4d7
Fabrice Bouyé <fabriceb@spc.int>
2016-11-22 14:53:08 +1100

    Added support for v2/wvw/abilities.

---
+++ src/api/web/gw2/mapping/v2/wvw/abilities/Ability.java
@@ -0,0 +1,59 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.wvw.abilities;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.SetValue;
+import api.web.gw2.mapping.core.URLReference;
+import api.web.gw2.mapping.core.URLValue;
+import api.web.gw2.mapping.v2.APIv2;
+import java.util.Set;
+
+/**
+ * Defines a WvW ability.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/wvw/abilities") // NOI18N.
+public interface Ability {
+
+    /**
+     * Gets the id of this ability.
+     * @return An {@code int}.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the localized name of this ability.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getName();
+
+    /**
+     * Gets the localized description of this ability.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getDescription();
+
+    /**
+     * Gets the URL to the icon of this ability.
+     * @return A {@code URLReference} instance, never {@code null}.
+     */
+    @URLValue
+    URLReference getIcon();
+
+    /**
+     * Gets a set of descriptions for each rank of this ability.
+     * @return A non-modifiable {@code Set<AbilityRank>} instance, never {@code null}.
+     */
+    @SetValue
+    Set<AbilityRank> getRanks();
+}
---
+++ src/api/web/gw2/mapping/v2/wvw/abilities/AbilityRank.java
@@ -0,0 +1,34 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.wvw.abilities;
+
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines a WvW ability rank.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/wvw/abilities") // NOI18N.
+public interface AbilityRank {
+
+    /**
+     * Gets the cost of this ability rank.
+     * @return An {@code int} &ge; 0.
+     */
+    @QuantityValue
+    int getCost();
+
+    /**
+     * Gets the localized effect of this ability rank.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getEffect();
+}
