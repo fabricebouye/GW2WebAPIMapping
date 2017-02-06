5cd2d831a32938483f311aeecf7e70d1646461ca
Fabrice Bouyé <fabriceb@spc.int>
2017-02-07 09:37:21 +1100

    Added support for v2/wvw/ranks.

---
+++ src/api/web/gw2/mapping/v2/wvw/ranks/Rank.java
@@ -0,0 +1,42 @@
+/* 
+ * Copyright (C) 2015-2017 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.wvw.ranks;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines a WvW rank.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/wvw/rank") // NOI18N.
+public interface Rank {
+
+    /**
+     * Gets the id of this WvW rank.
+     * @return An {@code int}.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the i18n title of this WvW rank.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getTitle();
+
+    /**
+     * Gets the min value of this WvW rank.
+     * @return An {@code int} &ge; 0.
+     */
+    @QuantityValue
+    int getMinRank();
+}
