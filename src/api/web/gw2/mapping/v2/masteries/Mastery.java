b46aa40d73c4fd19d86ab7e87271fed3881fc4ae
Fabrice Bouyé <fabriceb@spc.int>
2017-02-06 11:58:36 +1100

    Added support for v2/masteries.

---
+++ src/api/web/gw2/mapping/v2/masteries/Mastery.java
@@ -0,0 +1,75 @@
+/* 
+ * Copyright (C) 2015-2017 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.masteries;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.core.SetValue;
+import api.web.gw2.mapping.core.URLReference;
+import api.web.gw2.mapping.core.URLValue;
+import api.web.gw2.mapping.v2.APIv2;
+import java.util.Set;
+
+/**
+ * Defines a mastery.
+ * @author Fabrice Bouyé
+ * @see api.web.gw2.mapping.v2.account.masteries.AccountMastery
+ */
+@APIv2(endpoint = "v2/masteries") // NOI18N.
+public interface Mastery {
+
+    /**
+     * Gets the id of this mastery.
+     * @return An {@code int}.
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the I18N name of this mastery.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getName();
+
+    /**
+     * Gets the I18N requirement of this mastery.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getRequirement();
+
+    /**
+     * Gets the sort order of this mastery.
+     * @return An {@code int}.
+     */
+    @QuantityValue
+    int getOrder();
+
+    /**
+     * Gets the URL to the background of this mastery.
+     * @return An {@code URLValue} instance, never {@code null}.
+     */
+    @URLValue
+    URLReference getBackground();
+
+    /**
+     * Gets the id of the region in which this mastery is enabled.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @IdValue(flavor = IdValue.Flavor.STRING)
+    String getRegion();
+
+    /**
+     * Gets a set of all levels in this mastery.
+     * @return A non-modifiable {@code Set<MasteryLevel>}, never {@code null}.
+     */
+    @SetValue
+    Set<MasteryLevel> getLevels();
+}
---
+++ src/api/web/gw2/mapping/v2/masteries/MasteryLevel.java
@@ -0,0 +1,64 @@
+/* 
+ * Copyright (C) 2015-2017 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.masteries;
+
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.core.URLReference;
+import api.web.gw2.mapping.core.URLValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines a mastery level.
+ * @author Fabrice Bouyé
+ */
+@APIv2(endpoint = "v2/masteries") // NOI18N.
+public interface MasteryLevel {
+
+    /**
+     * Gets the I18N name of this mastery level.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getName();
+
+    /**
+     * Gets the I18N description of this mastery level.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getDescription();
+
+    /**
+     * Gets the I18N instruction of this mastery level.
+     * @return A {@code String} instance, never {@code null}.
+     */
+    @LocalizedResource
+    String getInstruction();
+
+    /**
+     * Gets the URL to the icon of this mastery level.
+     * @return An {@code URLValue} instance, never {@code null}.
+     */
+    @URLValue
+    URLReference getIcon();
+
+    /**
+     * Gets cost in mastery points for this mastery level.
+     * @return A {@code int} &gt; 0.
+     */
+    @QuantityValue
+    int getPointCost();
+
+    /**
+     * Gets cost in experience points for this mastery level.
+     * @return A {@code int} &gt; 0.
+     */
+    @QuantityValue
+    int getExpCost();
+}
