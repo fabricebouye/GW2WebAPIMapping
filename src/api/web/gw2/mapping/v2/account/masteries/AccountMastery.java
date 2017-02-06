95ea3532c9624ff19d8001b270dd57d652c92c76
Fabrice Bouyé <fabriceb@spc.int>
2017-02-06 11:58:57 +1100

    Added support for v2/account/masteries.

---
+++ src/api/web/gw2/mapping/v2/account/masteries/AccountMastery.java
@@ -0,0 +1,35 @@
+/* 
+ * Copyright (C) 2015-2017 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.account.masteries;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.v2.APIv2;
+
+/**
+ * Defines an account mastery unlock.
+ * @author Fabrice Bouyé
+ * @see api.web.gw2.mapping.v2.masteries.Mastery
+ */
+@APIv2(endpoint = "v2/account/masteries", requiresAuthentication = true, scope = "progression") // NOI18N.
+public interface AccountMastery {
+
+    /**
+     * Gets the id of the mastery.
+     * @return An {@code int}
+     */
+    @IdValue
+    int getId();
+
+    /**
+     * Gets the level of the mastery.
+     * @return A {@code int} &gt; 0.
+     */
+    @QuantityValue
+    int getLevel();
+}
