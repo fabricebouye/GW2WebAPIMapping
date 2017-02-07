eda400658e0387e728effd4ce5f90411f35545ac
Fabrice Bouyé <fabrice.bouye@gmail.com>
2017-02-07 22:07:00 +1100

    Added missing javadoc.

--- src/api/web/gw2/mapping/core/WebAPIException.java
+++ src/api/web/gw2/mapping/core/WebAPIException.java
@@ -18,19 +18,38 @@ public final class WebAPIException extends IOException {
 
     private final int code;
 
+    /**
+     * Creates a new instance.
+     * @param code The HTTP error code.
+     * @param content The content of the error, may be {@code null}.
+     */
     public WebAPIException(final int code, final String content) {
         super(content);
         this.code = code;
     }
 
+    /**
+     * Gets the HTTP error code.
+     * @return An {@code int}.
+     */
     public int getCode() {
         return code;
     }
 
+    /**
+     * Convenient factory method for HTTP 404 error.
+     * @param html The HTML content of the page.
+     * @return A {@code WebAPIException} instance, never {@code null}.
+     */
     public static WebAPIException of404(final String html) {
         return new WebAPIException(HttpURLConnection.HTTP_NOT_FOUND, html);
     }
 
+    /**
+     * Convenient factory method for HTTP 404 error.
+     * @param error The source error.
+     * @return A {@code WebAPIException} instance, never {@code null}.
+     */
     public static WebAPIException of403(final WebAPIError error) {
         return new WebAPIException(HttpURLConnection.HTTP_FORBIDDEN, (error == null) ? null : error.getText());
     }
